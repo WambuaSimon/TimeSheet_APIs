87adb4b52e50ab739141ab9af4b64f0696b27c3b
Gicheha <geoffreygicheha@gmail.com>
2019-01-10 13:31:24 +0300

    Database Seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Narse',
+            'email' => 'wambua433@gmail.com',
+            'password' => '1234'
+        ]);
+
+        DB::table('tasks')->insert([
+            'email' => 'wambua433@gmail.com',
+            'date'  => '12/12/2019',
+            'start_time' => '15/12/2019',
+            'end_time' => '27/12/2019',
+            'project' => 'timesheets',
+            'task' => 'Strike for no good reason'
+        ]);
+
+        DB::table('projects')->insert([
+            'name' => 'timesheets'
+        ]);
+
+
     }
 }
