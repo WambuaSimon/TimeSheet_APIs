ba20b670c92a3feb6b45cc65e1b1a65e24a72584
Narse <simonwambua433@gmail.com>
2019-01-14 16:13:11 +0300

    git ignore .env

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home',[
-            'tasks' => Tasks::simplePaginate(15)->get();
-        ]);
+        $tasks = Tasks::get()->simplePaginate(15);
+        return view('home',compact('tasks'));
     }
 }
