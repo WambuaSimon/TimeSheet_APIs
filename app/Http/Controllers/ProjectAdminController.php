b0da8043a9b8d1670a989ff20e2c552a9e0f60ad
Narse <simonwambua433@gmail.com>
2019-01-16 18:35:00 +0300

    user role to view users and projects

--- app/Http/Controllers/ProjectAdminController.php
+++ app/Http/Controllers/ProjectAdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Project;
-Use Auth;
+// Use Auth;
 
 class ProjectAdminController extends Controller
 {
--- app/Http/Controllers/UserAdminController.php
+++ app/Http/Controllers/UserAdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-Use Auth;
+// Use Auth;
 class UserAdminController extends Controller
 {
     /**
