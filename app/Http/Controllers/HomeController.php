3bcb6074ac0e677b7f87b9795d5237f239f432fc
Narse <simonwambua433@gmail.com>
2019-01-23 07:36:16 +0300

    added pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $tasks = Tasks::paginate(1);
+        $tasks = Tasks::paginate(10);
         // $tasks = Tasks::simplePaginate(15);
         return view('home',compact('tasks'));
     }
